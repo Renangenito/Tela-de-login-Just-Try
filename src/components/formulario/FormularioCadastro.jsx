9b3e7c4372c238051697b8ce2d427b1427a58aa4
renan lima <renangenito@hotmail.com>
2021-10-10 13:09:14 -0300

    Adicionando o  estilo a página do login

--- src/components/formulario/FormularioCadastro.jsx
+++ src/components/formulario/FormularioCadastro.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
-   
 const FormularioCadastro = () =>{
     return(
         <form className="formulario" action="">
-        <input className="inputEmail" type="email" name="email" id="" required placeholder="Digíte o seu email"/>
-        <input className="inputSenha" type="password" name="" id="" required placeholder="Digíte a sua senha"/>
+            <h4>Log in</h4>
+        <input className="inputEmail" type="email" name="email" id="" required placeholder="   Digíte o seu email"/>
+        <input className="inputSenha" type="password" name="" id="" required placeholder="  Digíte a sua senha"/>
         <button className="botao" type="submit">Login</button>
+        
+        <div className="botao-linkedin">
+        <FaLinkedinIn className="icones"/>  <p className="texto-icone">SIGN IN WITH LINKEDIN</p>
+        </div>
+       <div className="botao-instagram">
+        <FaGithub className="icones"/> <p className="texto-icone">SIGN IN WITH GITHUB</p>
+       </div>
+      
     </form>
     )
     
